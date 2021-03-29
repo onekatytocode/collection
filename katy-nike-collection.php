4ca644a272454f16760f9b7f613a230fc872abf4
onekatytocode <1katytogo@gmail.com>
2021-03-29 14:38:18 +0100

    changes to db connection made

--- katy-nike-collection.php
+++ katy-nike-collection.php
@@ -1,1 +1,11 @@
 <?php
+/**
+ * retrieves a DB connection to the collection of shoes DB
+ */
+$db = new PDO('mysql:host=db; dbname=katy-collection', 'root', 'password');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+return $db;
+    
+    echo '<pre>';
+var_dump($result);
+echo '</pre>';
