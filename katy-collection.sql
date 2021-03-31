55a780b234ed984d32085fddf7bf4eb70f39f8a9
onekatytocode <1katytogo@gmail.com>
2021-03-31 16:17:06 +0100

    changed datetype in images

--- katy-collection.sql
+++ katy-collection.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.33)
 # Database: katy-collection
-# Generation Time: 2021-03-31 14:57:31 +0000
+# Generation Time: 2021-03-31 15:15:24 +0000
 # ************************************************************
 
 
@@ -32,7 +32,7 @@ CREATE TABLE `sneakers` (
   `year-released` year(4) DEFAULT NULL,
   `color-way` varchar(200) DEFAULT NULL,
   `size` varchar(10) DEFAULT '',
-  `images` blob,
+  `images` varchar(255) DEFAULT '',
   `price` int(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -42,12 +42,12 @@ LOCK TABLES `sneakers` WRITE;
 
 INSERT INTO `sneakers` (`id`, `name`, `year-released`, `color-way`, `size`, `images`, `price`)
 VALUES
-	(1,'Neon -nike','2011','black / neon green','4-13',X'696D616765732F6E656F6E6E696B652E706E67',1550),
-	(2,'Back To The Future -nike','2020','grey / pink /blue','5-11',X'696D616765732F627474666E696B652E706E67',2250),
-	(3,'Gold -nike','2015','pure gold','7-13',X'696D616765732F676F6C646E696B652E706E67',5590),
-	(4,'TMNT -nike','2018','green / red - green / blue - green / orange - green / purple','3-10',X'696D616765732F746D6E746E696B652E706E67',1220),
-	(5,'Darth -nike','2019','black / white','6-13',X'696D616765732F64617274686E696B652E706E67',1300),
-	(6,'Ben and Jerrys -nike','2021','multi','3-12',X'696D616765732F62656E6A657272796E696B652E706E67',1900);
+	(1,'Neon -nike','2011','black / neon green','4-13','images/neonnike.png',1550),
+	(2,'Back To The Future -nike','2020','grey / pink /blue','5-11','images/bttfnike.png',2250),
+	(3,'Gold -nike','2015','pure gold','7-13','images/goldnike.png',5590),
+	(4,'TMNT -nike','2018','green / red - green / blue - green / orange - green / purple','3-10','images/tmntnike.png',1220),
+	(5,'Darth -nike','2019','black / white','6-13','images/darthnike.png',1300),
+	(6,'Ben and Jerrys -nike','2021','multi','3-12','images/benjerrynike.png',1900);
 
 /*!40000 ALTER TABLE `sneakers` ENABLE KEYS */;
 UNLOCK TABLES;
